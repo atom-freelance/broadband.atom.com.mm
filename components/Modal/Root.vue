d24e16c0b9cad5916feffdf433854679b5c03830
mac <mac@macs-MacBook-Pro.local>
2024-01-28 11:17:09 +0630

    feature: relocate modal box

--- components/Modal/Root.vue
+++ components/Modal/Root.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 const props = defineProps({
 	id: { type: String, required: true },
+	isFullWidth: {
+		type: Boolean,
+		required: false,
+		default: false,
+	},
 })
 </script>
 
 <template>
 	<dialog :id="props.id" class="modal">
-		<div class="modal-box py-0">
+		<div :class="[{ 'min-w-full': props.isFullWidth }, 'modal-box py-0']">
 			<slot />
 		</div>
 	</dialog>
