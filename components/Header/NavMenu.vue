7844986aa39b1472643134bda4acd3b54dea0d3e
wai-lin <wailin.wilbert@gmail.com>
2024-02-05 21:32:01 +0630

    refactor: util file naming, update core packages

--- components/Header/NavMenu.vue
+++ components/Header/NavMenu.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-const navbarLinks = [
-	{ type: 'link', title: 'Home', to: '/' },
-	{ type: 'link', title: 'Shop', to: '/shop' },
-	{ type: 'label', title: 'FAQs', subLinks: FAQLinks },
-	{ type: 'link', title: 'Change ownership', to: '/change_ownership' },
-]
-</script>
-
 <template>
 	<ul class="ml-20 hidden h-full space-x-8 lg:flex lg:items-center">
 		<li
---
+++ utils/navbarLinks.ts
@@ -0,0 +1,6 @@
+export const navbarLinks = [
+	{ type: 'link', title: 'Home', to: '/' },
+	{ type: 'link', title: 'Shop', to: '/shop' },
+	{ type: 'label', title: 'FAQs', subLinks: FAQLinks },
+	{ type: 'link', title: 'Change ownership', to: '/change_ownership' },
+]
