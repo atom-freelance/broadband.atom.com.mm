716711d8a24d8c133caa7327b3b2a70be409adb7
mac <mac@macs-MacBook-Pro.local>
2024-02-05 19:31:01 +0630

    feature: dashboard item hover effect

--- components/Dashboard/BenefitItem.vue
+++ components/Dashboard/BenefitItem.vue
@@ -2,35 +2,36 @@
 import { twMerge } from 'tailwind-merge'
 
 const props = defineProps({
-	icon: {
-		type: String as PropType<IconName>,
-		required: false,
-		default: 'sim-card',
-	},
-	title: {
-		type: String as PropType<string>,
-		required: true,
-	},
-	subTitle: {
-		type: String as PropType<string>,
-		required: true,
-	},
+    icon: {
+        type: String as PropType<IconName>,
+        required: false,
+        default: 'sim-card',
+    },
+    title: {
+        type: String as PropType<string>,
+        required: true,
+    },
+    subTitle: {
+        type: String as PropType<string>,
+        required: true,
+    },
 })
 </script>
 
 <template>
-	<li class="flex flex-wrap items-center justify-between">
-		<div class="flex items-center gap-2">
-			<span class="rounded-full bg-gray-200 p-3"
-				><Icon :name="props.icon" />
-			</span>
+    <li
+        class="flex flex-wrap items-center justify-between hover:cursor-pointer hover:bg-gray-300 rounded-lg p-4 transition ease-in-out duration-300">
+        <div class="flex items-center gap-2">
+            <span class="rounded-full bg-gray-200 p-3">
+                <Icon :name="props.icon" />
+            </span>
 
-			<div class="flex flex-col items-start justify-center gap-1">
-				<span class="font-bold">{{ props.title }}</span>
-				<span class="text-sm">{{ props.subTitle }}</span>
-			</div>
-		</div>
+            <div class="flex flex-col items-start justify-center gap-1">
+                <span class="font-bold">{{ props.title }}</span>
+                <span class="text-sm">{{ props.subTitle }}</span>
+            </div>
+        </div>
 
-		<slot />
-	</li>
+        <slot />
+    </li>
 </template>
